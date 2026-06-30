cf12be05b25dec8076e52ac54afe02bdbfd7b81f
hadimitrapratama031-lab <hadimitrapratama031@gmail.com>
2026-06-30 14:19:13 +0700

    fix: set correct supabase URL and ignore node_modules

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,7 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'GANTI_DENGAN_SUPABASE_URL';
+const SUPABASE_URL = 'https://dqiayrsojpyglkumtsgo.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxaWF5cnNvanB5Z2xrdW10c2dvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3OTI2MTYsImV4cCI6MjA5ODM2ODYxNn0.TTueIeVBoxW_3cNuuj6JdKuiQSiqDa0g2bTutfe9Oqg';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
